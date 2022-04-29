bdbbd48fc2db8bd19113754f430b2add4491c5df
hayitsdavid <davehay93@gmail.com>
2022-04-28 18:38:34 -0700

    box with css + started js

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-document.getElementsByClassName('container');
+const container = document.getElementsByClassName('container');
 
 const makeSquares = () => {
+
+}
+
+const player1 = () => {
+
+}
+
+const player2 = () => {
 
 }
